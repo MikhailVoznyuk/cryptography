e06b8b3abd2a7569c074a2a395b8ab9c21b3a4e8
Mikhail Voznyuk <mikhailv.commerce@gmail.com>
2025-06-10 18:21:15 +0300

    v0.01

--- app/page.js
+++ app/page.js
@@ -1,50 +1,83 @@
+'use client'
+
+import React from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  function crypt(content, setWindowContent) {
+    const type = cryptType;
+    const fromAlph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ .,!:;?-'.split('');
+    const toAlph = 'VWXYZ .,!:;?-KLMNOPQRSTUABCDEFGHIJ'.split('');
+    let newContent = '';
+    console.log(fromAlph, toAlph)
+    let fromMap = new Map();
+    let toMap = new Map();
+    for (let i = 0; i < fromAlph.length; i++) {
+      toMap.set(fromAlph[i], toAlph[i]);
+    }
+    for (let i = 0; i < toAlph.length; i++) {
+      fromMap.set(toAlph[i], fromAlph[i]);
+    }
+    content = content.toUpperCase();
+    
+    if (type == 'to') {
+      for (let s of content) {
+        newContent += (toMap.get(s) == undefined) ? ' ' : toMap.get(s);
+      
+      }
+    } else {
+      for (let s of content) {
+        newContent += (fromMap.get(s) == undefined) ? ' ' : fromMap.get(s) ;
+      }
+    }
+  
+    setWindowContent(newContent);
+  }
+  const [encryptContent, setEncryptContent] = React.useState('');
+  const [baseContent, setBaseContent] = React.useState('');
+  const [cryptType, setCryptType] = React.useState('to');
+  const [cryptFunction, setCryptFunction] = React.useState(0);
+  const formRef = React.useRef();
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.container}>
+          <div className={styles.col}>
+            <form className={styles.mainForm} action={crypt}>
+              <select name="type" onChange={e => {
+                setCryptType(e.target.value);
+                formRef.current.value = '';
+              }}>
+                <option value="to">Шифрование</option>
+                <option value="from">Дешифрование</option>
+              </select>
+              <select name='cryptFunc' onchange={e => {
+                const cryptFunc = e.current.value;
+                setCryptFunction(+cryptFunction);
+              }}>
+                <option value='0'>Перестановка</option>
+                <option value='1'>Что-то еще</option>
+              </select>
+              <textarea ref={formRef} onChange={(e) => {
+     
+                
+                crypt(e.target.value, setEncryptContent)}
+                } name="content" placeholder="Введите сообщение"></textarea>
+   
+            </form>
+          </div>
+          <div className={styles.col}>
+            { encryptContent ? (<div className={styles.resultContainer}>
+                <p>{encryptContent}</p>
+              </div>) : null
+            }
+          </div>
+          
         </div>
+
+      
       </main>
       <footer className={styles.footer}>
         <a
