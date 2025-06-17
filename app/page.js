85c9d48fa8da834cea639efa89ebf38821e1dd06
Mikhail Voznyuk <mikhailv.commerce@gmail.com>
2025-06-17 13:13:58 +0300

    v0.03

--- app/page.js
+++ app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
   const [baseContent, setBaseContent] = React.useState('');
   const [toCrypt, setToCrypt] = React.useState(true);
   const [cryptFuncType, setCryptFuncType] = React.useState(0);
+  const [cryptKey, setCryptKey] = React.useState(null);
   const formRef = React.useRef();
   return (
     <div className={styles.page}>
@@ -35,8 +36,16 @@ export default function Home() {
                 <option value='0'>Перестановка</option>
                 <option value='1'>Шифр Фейнстеля</option>
               </select>
+              <>
+              { (cryptFuncType == 1) ? 
+                <input placeholder="Введите ключ" onChange={(e) => {
+                  setCryptKey(e.target.value)
+                }}></input>
+                : null
+              }
+              </>
               <textarea ref={formRef} onChange={(e) => {
-                  crypt(e.target.value, cryptFuncType, toCrypt, setEncryptContent)}
+                  crypt(e.target.value, cryptFuncType, toCrypt, setEncryptContent, cryptKey)}
                 } name="content" placeholder="Введите сообщение"></textarea>
    
             </form>
@@ -53,48 +62,7 @@ export default function Home() {
       
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      
       </footer>
     </div>
   );
